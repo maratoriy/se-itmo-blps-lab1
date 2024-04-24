2102e385e92babde7a9cd753901fc9b699c1f23f
moratorium <spgamermdk@gmail.com>
2024-04-24 05:26:50 +0300

    Fixed embedded postgres dependency and added simple test

--- src/test/java/moratorium/se/itmo/blps/SeItmoBlpsLab1ApplicationTests.java
+++ src/test/java/moratorium/se/itmo/blps/SeItmoBlpsLab1ApplicationTests.java
@@ -1,9 +1,12 @@
 package moratorium.se.itmo.blps;
 
+import moratorium.se.itmo.blps.config.EmbeddedDatasourceConfiguration;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration(classes = {EmbeddedDatasourceConfiguration.class})
 class SeItmoBlpsLab1ApplicationTests {
 
 	@Test
